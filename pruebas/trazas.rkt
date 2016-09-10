72215dd250923b5b9462862c25769d2506c4642a
Sara*er <saraer90@gmail.com>
2016-09-10 04:34:41 +0200

    Eliminar archivo de trazas

--- pruebas/trazas.rkt
+++
@@ -1,7 +0,0 @@
-(require "../program.rkt")
-(require "../lib/utils.rkt")
-(require "../lib/UI.rkt")
-
-(define pairs (mcons (mcons 1 2) (mcons 3 4)))
-
-(box-pointer pairs)
--- saves/canvas.rkt
+++ saves/canvas.rkt
@@ -1,1 +1,1 @@
-(define pairs (mcons (mcons (mcons "1" "2") (mcons "1" "2") ) (mcons (mcons "1" "2") (mcons "1"  '[loop] ) ) ) )
+(define pairs (mcons (mcons 1 2) (mcons 3 4) ) )
